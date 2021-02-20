79c507efa0cc440e5f43c036e16289782d58bc75
tuxuser <462620+tuxuser@users.noreply.github.com>
2021-02-20 08:14:38 +0100

    PCAP parser: Add skeleton parsing code

--- xcloud/scripts/pcap_reader.py
+++ xcloud/scripts/pcap_reader.py
@@ -3,9 +3,32 @@ PCAP Parser for XCloud network traffic
 """
 import argparse
 
+import dpkt
+from construct.lib import containers
 
-def parse_file(filepath: str) -> None:
-    print(f"hello -> {filepath}")
+
+containers.setGlobalPrintFullStrings(True)
+
+
+def packet_filter(filepath):
+    with open(filepath, 'rb') as fh:
+        for ts, buf in dpkt.pcap.Reader(fh):
+            eth = dpkt.ethernet.Ethernet(buf)
+
+            # Make sure the Ethernet data contains an IP packet
+            if not isinstance(eth.data, dpkt.ip.IP):
+                continue
+
+            ip = eth.data
+            if not isinstance(ip.data, dpkt.udp.UDP):
+                continue
+
+            yield(ip.data, ts)
+
+
+def parse_file(pcap_filepath: str) -> None:
+    for packet, timestamp in packet_filter(pcap_filepath):
+        print(packet, timestamp)
 
 
 def main():
