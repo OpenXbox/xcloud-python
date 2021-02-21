c690b16501e6899847de99a43edcb68c4d978b77
tuxuser <462620+tuxuser@users.noreply.github.com>
2021-02-21 14:46:26 +0100

    Print basic RTP/STUN packet info

--- xcloud/scripts/pcap_reader.py
+++ xcloud/scripts/pcap_reader.py
@@ -2,13 +2,35 @@
 PCAP Parser for XCloud network traffic
 """
 import argparse
+import logging
 
 import dpkt
+from aiortc import rtp
+from aioice import stun
 from construct.lib import containers
 
+from ..protocol import packets
 
+
+logging.basicConfig(level=logging.DEBUG)
 containers.setGlobalPrintFullStrings(True)
+LOG = logging.getLogger(__name__)
 
+def print_stun(stun: stun.Message) -> None:
+    print(f'STUN: {stun}')
+
+def print_rtp(rtp: rtp.RtpPacket) -> None:
+    try:
+        payload_name = packets.PayloadType(rtp.payload_type)
+    except:
+        payload_name = '<UNKNOWN>'
+
+    print(f'RTP: {payload_name.name} {rtp}')
+
+PACKET_TYPES = [
+    (stun.parse_message, print_stun),
+    (rtp.RtpPacket.parse, print_rtp)
+]
 
 def packet_filter(filepath):
     with open(filepath, 'rb') as fh:
@@ -28,8 +50,14 @@ def packet_filter(filepath):
 
 def parse_file(pcap_filepath: str) -> None:
     for packet, timestamp in packet_filter(pcap_filepath):
-        print(packet, timestamp)
-
+        packet = bytes(packet.data)
+        for cls, print_func in PACKET_TYPES:
+            try:
+                instance = cls(packet)
+                print_func(instance)
+            except:
+                pass
+        
 
 def main():
     parser = argparse.ArgumentParser(
